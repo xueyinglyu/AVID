b664f26a1f355fb7a044a339f8f5641c89413b85
xueyinglyu <78334297+xueyinglyu@users.noreply.github.com>
2025-07-09 14:42:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ dest=$PWD
 echo $dest
 #mkdir -p $dest/3rdParty
 conda create -p $dest/3rdParty -c bioconda -c conda-forge -y bwa star blast samtools python
-$dest/3rdParty/bin/pip install numpy pandas pysam
+$dest/3rdParty/bin/pip install numpy==2.3.0 pandas pysam
 conda create -p $dest/R -c bioconda -c conda-forge -y r r-circlize r-base r-ggplot2 r-plyr
  
 
